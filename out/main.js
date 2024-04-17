e4a74c16733c4af16e5e8270837052fbcf189006
sare20090120 <140720813+sare20090120@users.noreply.github.com>
2024-04-17 08:45:18 +0000

    punch or kick me

---
+++ out/main.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Click = void 0;
+function Click() {
+    alert("More Please!!!!!!");
+}
+exports.Click = Click;
